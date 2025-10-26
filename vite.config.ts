8cac9adb5041e437668d30c9e3eab8b476576621
itray25 <314118@qq.com>
2025-10-26 17:26:35 +0800

    chore: project init

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import eslint from "vite-plugin-eslint2";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    eslint({
+      lintOnStart: true,
+      cache: false,
+    }),
+  ],
+});
